f10b370de58bf2632c9cf56e16e1a9c6d3c306cc
MeillieA <meilliez.antoine@gmail.com>
2022-03-03 10:13:53 +0100

    Add DISTANCES constant and remove ray

--- KBNPathfinder/metrics/distances.py
+++ KBNPathfinder/metrics/distances.py
@@ -1,16 +1,15 @@
 import numpy as np
-import ray
 from scipy.spatial import distance
-from typing import Type
+from typing import Type, Callable, Dict
 
 from KBNPathfinder.structures.graph import Node
 
 
-@ray.remote
 def euclidian_distance(node1: Type[Node], node2: Type[Node]) -> np.float64:
     vector1, vector2 = [node1.x, node1.y], [node2.x, node2.y]
     return distance.euclidean(vector1, vector2)
 
 
-
-
+DISTANCES: Dict[str: Callable] = {
+    "euclidian": euclidian_distance
+}
