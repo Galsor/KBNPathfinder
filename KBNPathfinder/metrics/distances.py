19c444a177ce01f735af4f06e6d117b7ec5ce0fd
MeillieA <meilliez.antoine@gmail.com>
2022-03-03 10:29:37 +0100

    Add distance registering

--- KBNPathfinder/metrics/distances.py
+++ KBNPathfinder/metrics/distances.py
@@ -1,15 +1,34 @@
 import numpy as np
 from scipy.spatial import distance
-from typing import Type, Callable, Dict
+from typing import Type, Callable, Dict, Any
 
-from KBNPathfinder.structures.graph import Node
+from KBNPathfinder.structures.node import Node
+
+_DISTANCES: Dict[str, Callable] = {}
 
 
+def register_distance(name: str) -> Callable:
+    def decorator(fun: Callable) -> Callable:
+        _DISTANCES[name] = fun
+
+        def wrapper(*args, **kwargs) -> Any:
+            return fun(*args, **kwargs)
+        return wrapper
+    return decorator
+
+
+def get_distance(name: str) -> Callable:
+    try:
+        distance_fun = _DISTANCES[name]
+        return distance_fun
+    except KeyError:
+        raise ValueError(f"Invalid distance name with {name}. Implemented distances are: [{', '.join(_DISTANCES.keys())}]")
+
+
+@register_distance("euclidian")
 def euclidian_distance(node1: Type[Node], node2: Type[Node]) -> np.float64:
     vector1, vector2 = [node1.x, node1.y], [node2.x, node2.y]
     return distance.euclidean(vector1, vector2)
 
 
-DISTANCES: Dict[str: Callable] = {
-    "euclidian": euclidian_distance
-}
+
