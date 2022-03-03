80c8388fa008d240f2aa5324b918fa29e3447ea3
MeillieA <meilliez.antoine@gmail.com>
2022-03-03 10:39:39 +0100

    Add tests for distances

---
+++ test/unit/metrics/test_distances.py
@@ -0,0 +1,28 @@
+import math
+
+from KBNPathfinder.metrics.distances import register_distance, _DISTANCES, get_distance, euclidian_distance
+from KBNPathfinder.structures.node import Node
+
+
+def test_register_distance():
+    distance_name = "test_distance"
+    @register_distance(distance_name)
+    def null_distance():
+        return 0
+
+    assert "test_distance" in _DISTANCES
+
+
+def test_get_distance():
+    distance_name = "test_distance"
+    _DISTANCES[distance_name] = lambda x: 42
+
+    distance_fun = get_distance(distance_name)
+
+    assert distance_fun(None) == 42
+
+
+def test_euclidian_distance():
+    node1, node2 = Node(id=0,  x=1, y=0, score=0), Node(id=1, x=0, y=1, score=0)
+    distance = euclidian_distance(node1, node2)
+    assert distance == math.sqrt(2)
