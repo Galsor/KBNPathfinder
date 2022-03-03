62a7edccf0dda2d9af12f8a0d860223965c2e3b9
MeillieA <meilliez.antoine@gmail.com>
2022-03-03 12:15:00 +0100

    Adapt RandomGraph to the new KBNGraph interface

--- KBNPathfinder/random_graph.py
+++ KBNPathfinder/random_graph.py
@@ -7,20 +7,18 @@ from KBNPathfinder.structures.graph import KBNGraph, Node
 
 class RandomGraph(KBNGraph):
     def __init__(
-        self, n: int = 100, max_cost: float = 0.4, random_seed: Optional[int] = None
+        self, n: int = 100, max_cost: float = 0.4, distance: str = "euclidian", edge_cost_offset: float = 0.0, random_seed: Optional[int] = None
     ):
         np.random.seed(random_seed)
-        self.max_cost = max_cost
-        self.nodes = self.gen_rand_node(n=n)
-        self.neighborhood = {node_id: [] for node_id in self.nodes.keys()}
-        self.edges = self.build_egdes(n=n)
+        nodes_list = self.gen_rand_node(n=n)
+        super(RandomGraph, self).__init__(nodes_list, distance = distance, max_cost= max_cost, edge_cost_offset=edge_cost_offset)
 
     @staticmethod
     def gen_rand_node(n=10) -> List[Node]:
-        nodes = {}
+        nodes = []
         x = np.random.rand(n)
         y = np.random.rand(n)
         v = np.random.randint(0, 100, n)
         for i in range(n):
-            nodes[i] = Node(id=i, x=x[i], y=y[i], score=v[i])
+            nodes.append(Node(id=i, x=x[i], y=y[i], score=v[i]))
         return nodes
