faacf7bd32b19cc9bd705cbd87ad6cb9967bf752
MeillieA <meilliez.antoine@gmail.com>
2022-03-15 11:37:12 +0100

    Add find_best_regions convolution function

---
+++ src/spatial/convolutions.py
@@ -0,0 +1,6 @@
+from src.structures.graph import KBNGraph, KBNSubGraph
+
+def find_best_regions(graph: KBNGraph, k: int) -> KBNSubGraph:
+    # TODO: Convolv along coordinates to find region when the sum k best nodes score is the highest.
+    #  Return a subgraph with all nodes in the region.
+    raise NotImplementedError
