f4412057abcc5bcc81843fd6dc7a2e47a78a8f41
MeillieA <meilliez.antoine@gmail.com>
2022-03-15 13:47:53 +0100

    add node properties to pandas builder

--- src/utils/builders.py
+++ src/utils/builders.py
@@ -11,14 +11,13 @@ def build_nodes_from_pandas(
     x_col: str,
     y_col: str,
     id_col: Optional[str] = None,
+    properties_cols: List[str] = []
 ) -> List[Node]:
-    selected_cols = [x_col, y_col, score_col]
-    if id_col is not None:
-        selected_cols.append(id_col)
 
-    nodes_values = df[selected_cols].values
     nodes = []
-    for i, row in enumerate(nodes_values):
+    for i, row in df.iterrows():
+        properties = row[properties_cols].to_dict()
         id_value = row[3] if id_col is not None else i
-        nodes.append(Node(id=id_value, x=row[0], y=row[1], score=row[2]))
+        nodes.append(Node(id=id_value, x=row[x_col], y=row[y_col], score=row[score_col], properties=properties))
     return nodes
+
