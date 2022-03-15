6b5e4fed6777cc4d0ee49ee44eb5081a502a7316
MeillieA <meilliez.antoine@gmail.com>
2022-03-15 15:51:28 +0100

    update random_graph tests to test properties

--- test/unit/test_random_graph.py
+++ test/unit/test_random_graph.py
@@ -5,7 +5,9 @@ from src.random_graph import RandomGraph
 
 def test_random_graph():
     g = RandomGraph(n=10)
+    expected_properties = ["num", "cat"]
 
     assert len(g.nodes) == 10
     assert len(g.neighborhood) == 10
     assert g.edges
+    assert all([prop in node.properties for node in g.nodes.values() for prop in expected_properties])
