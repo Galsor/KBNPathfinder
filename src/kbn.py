11b08d8853a566b17621a9a2fb79384b85d197e1
MeillieA <meilliez.antoine@gmail.com>
2022-04-05 10:33:22 +0200

    Add convolution initialisation in KBN algorithm

--- src/kbn.py
+++ src/kbn.py
@@ -4,18 +4,12 @@ from typing import List, Optional, Type
 
 from src.contraints.base import BaseConstraint
 from src.contraints.update import revert_constraints_for_all, update_constraints
+from src.initialisations import KBN_INITIALISATIONS
+from src.spatial.convolutions import get_subgraphs_nodes_list_from_k_best_nodes_convolution
 from src.structures.graph import KBNGraph, Node
+from src.utils.iterator import SubgraphIterator
 
 logger = logging.getLogger(__name__)
-
-
-def get_k_best_nodes(graph: KBNGraph, first_node: Node, k: int = 10) -> List[Node]:
-    """Return the best chain of k nodes. Starting with the node of max score"""
-    # TODO: Add initialisation methods
-    # - Start_node = Node with Max Score
-    # - Best regional score
-    k_best_nodes_list = find_next_best_neighbors(graph, [first_node], k - 1)
-    return k_best_nodes_list
 
 
 def get_groups_of_k_best_nodes_from_max_score(
@@ -24,29 +18,56 @@ def get_groups_of_k_best_nodes_from_max_score(
     n_groups: int = 5,
     constraints: List[BaseConstraint] = [],
     max_iter: Optional[int] = None,
+    initialisation: str = "max_score",
+    *args,
+    **kwargs
 ) -> List[List[Type[Node]]]:
-    def dispatch_k(total_k, n_groups) -> List[int]:
-        base, rest = total_k // n_groups, total_k % n_groups
-        k_values = [base for _ in range(n_groups)]
-        for i in range(rest):
-            k_values[i % n_groups] += 1
-        return k_values
+    logger.info(f"Main graph information -> Shape:{graph.shape}. Connectivity:{graph.connectivity}")
 
-    results = []
-    k_values = dispatch_k(total_k, n_groups)
+    k_values = dispatch_k_in_n_groups(total_k, n_groups)
+    subgraphs_nodes_ids = get_subgraphs_nodes_list_from_k_best_nodes_convolution(
+        graph, k=max(k_values), *args, **kwargs
+    )
+    subgraph_gen = SubgraphIterator(graph, subgraphs_nodes_ids)
+
     i = 0
+    results = []
     max_iter = max_iter if max_iter is not None else n_groups * 3
     while len(results) < n_groups or i < max_iter:
         logger.info(f"Starting optimisation for group {i+1}")
-        candidates = get_k_best_nodes_from_max_score(
-            graph, k=k_values[len(results)], constraints=constraints
+        k = k_values[len(results)]
+
+        subgraph = next(subgraph_gen)
+        logger.info(f"Selected subgraph information -> Shape:{subgraph.shape}. Connectivity:{subgraph.connectivity}")
+
+        init_node = KBN_INITIALISATIONS[initialisation](
+            subgraph, k=k, constraints=constraints
         )
-        if len(candidates) == k_values[i]:
+        logger.info(f"Node selected for initialisation: {init_node}")
+        candidates = get_k_best_nodes(subgraph, init_node, k=k-1)
+        logger.info(f"Candidates selected ({len(candidates)}): {candidates}")
+        if len(candidates) >= k:
             results.append(candidates)
         else:
             revert_constraints_for_all(constraints, candidates)
         i += 1
     return results
+
+
+def dispatch_k_in_n_groups(total_k, n_groups) -> List[int]:
+    base, rest = total_k // n_groups, total_k % n_groups
+    k_values = [base for _ in range(n_groups)]
+    for i in range(rest):
+        k_values[i % n_groups] += 1
+    return k_values
+
+
+def get_k_best_nodes_from_convolution(
+        graph: KBNGraph, k: int, constraints: List[BaseConstraint] = []
+) -> List[Type[Node]]:
+    subgraph = get_subgraphs_nodes_list_from_k_best_nodes_convolution(graph, k, window_overlap=0.9)
+    k_best_nodes_list = get_k_best_nodes_from_max_score(subgraph, k, constraints)
+    return k_best_nodes_list
 
 
 def get_k_best_nodes_from_max_score(
@@ -56,6 +77,11 @@ def get_k_best_nodes_from_max_score(
     k_best_nodes_list = find_next_best_neighbors(graph, [best_node], k - 1, constraints)
     return k_best_nodes_list
 
+
+def get_k_best_nodes(graph: KBNGraph, first_node: Node, k: int = 10) -> List[Node]:
+    """Return the best chain of k nodes. Starting with the node of max score"""
+    k_best_nodes_list = find_next_best_neighbors(graph, [first_node], k - 1)
+    return k_best_nodes_list
 
 def find_next_best_neighbors(
     graph: KBNGraph,
@@ -71,11 +97,14 @@ def find_next_best_neighbors(
     next_node = get_neighboor_with_max_regional_score(
         graph, last_node.id, node_count_to_add, constraints
     )
-    logger.info("Node selection duration:", round(time.time() - t0, 2), "s")
-    logger.info("Node selected:", next_node)
+
+    logger.info(f"Node selection duration: {round(time.time() - t0, 2)}s")
+    logger.info(f"Node selected: {next_node}")
+
     if next_node is not None:
         selected_nodes.append(next_node)
         graph.deactivate_node(last_node.id)
+
     if node_count_to_add > 1:
         # Pursue recursion
         node_count_to_add -= 1
@@ -85,6 +114,7 @@ def find_next_best_neighbors(
     elif next_node is not None:
         graph.deactivate_node(next_node.id)
         update_constraints(constraints, next_node)
+
     return selected_nodes
 
 
