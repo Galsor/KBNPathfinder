ed7eb138f396999548bf3254a3d2b2ed84412aa4
MeillieA <meilliez.antoine@gmail.com>
2022-04-05 10:32:39 +0200

    Add optional arguments and Dict of available initialisations

--- src/initialisations.py
+++ src/initialisations.py
@@ -1,14 +1,22 @@
+from typing import Dict, Callable
+
 from src.spatial.find import get_closest_node_id
 from src.structures.graph import KBNGraph
 from src.structures.node import Node
 
 
-def from_node_with_max_score(graph: KBNGraph) -> Node:
+def from_node_with_max_score(graph: KBNGraph,  *args, **kwargs) -> Node:
     return graph.get_node_with_max_score()
 
 
-def from_closest_node(graph: KBNGraph, x: float, y: float) -> Node:
+def from_closest_node(graph: KBNGraph, x: float, y: float, *args, **kwargs) -> Node:
     nodes_coords_df = graph.coordinates()
     closest_node_id = get_closest_node_id(coordinates=nodes_coords_df, x=x, y=y)
     node = graph.nodes[closest_node_id]
     return node
+
+
+KBN_INITIALISATIONS: Dict[str, Callable] = {
+    "max_score": from_node_with_max_score,
+    "closest": from_closest_node,
+}
