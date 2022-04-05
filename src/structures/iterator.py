76507c9cc61cfc662d0061bb275dea39e7cc418c
MeillieA <meilliez.antoine@gmail.com>
2022-04-05 10:20:33 +0200

    Type hints

--- src/structures/iterator.py
+++ src/structures/iterator.py
@@ -2,12 +2,12 @@ from dataclasses import dataclass
 from typing import List
 
 from src.structures.graph import KBNSubGraph, KBNGraph
-from src.structures.node import Node
+
 
 @dataclass
 class SubgraphGenerator:
     graph: KBNGraph
-    node_list: List[Node]
+    node_list: List[int]
 
     def __next__(self) -> KBNSubGraph:
         return KBNSubGraph(self.graph, self.node_list)
